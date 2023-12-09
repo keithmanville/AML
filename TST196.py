70c61167761fa4138f489908de197b788be03332
Oaramoon <71409778+oaramoon@users.noreply.github.com>
2023-12-09 10:27:07 -0500

    Update TST196.py

--- TST196.py
+++ TST196.py
@@ -45,3 +45,48 @@ def load_cifar10_model(model_path):
     checkpoint = tf.train.Checkpoint(model=model)
     checkpoint.restore(model_path)
     return model
+
+
+def augment(x, y):
+    x_shape = tf.shape(x)
+    x = tf.image.random_flip_left_right(x)
+    x = tf.pad(x, [[0] * 2, [4] * 2, [4] * 2, [0] * 2], mode='REFLECT')
+    return tf.image.random_crop(x, x_shape), y
+
+
+def augment_strong(image,
+                   strength=.5):
+    # Color jitter taken from SimCLR implementation
+
+    brightness = 0.8 * strength
+    contrast = 0.8 * strength
+    saturation = 0.8 * strength
+    hue = 0.2 * strength
+    def apply_transform(i, x):
+        def brightness_foo():
+            return tf.image.random_brightness(x, max_delta=brightness)
+        
+        def contrast_foo():
+            return tf.image.random_contrast(x, lower=1-contrast, upper=1+contrast)
+        
+        def saturation_foo():
+            return tf.image.random_saturation(
+                x, lower=1-saturation, upper=1+saturation)
+  
+        def hue_foo():
+            return tf.image.random_hue(x, max_delta=hue)
+        
+        x = tf.cond(tf.less(i, 2),
+                    lambda: tf.cond(tf.less(i, 1), brightness_foo, contrast_foo),
+                    lambda: tf.cond(tf.less(i, 3), saturation_foo, hue_foo))
+        return x
+  
+    perm = tf.random.shuffle(tf.range(4))
+    for i in range(4):
+        image = apply_transform(perm[i], image)
+        image = tf.clip_by_value(image, 0., 1.)
+    return augment(image, None)[0]
+
+
+def augment_strong_np(image, strength=.5):
+    return augment_strong(image, strength).numpy()
